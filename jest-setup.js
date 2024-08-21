0da0d297f46931338c284d85b15f43fe75dd1f48
Erick Gomez <erick0gomez98@gmail.com>
2024-08-20 20:58:09 -0600

    gooooooodddd

--- jest-setup.js
+++ jest-setup.js
@@ -3,10 +3,10 @@ const mockGestureHandler = jest.requireMock('react-native-gesture-handler');
 
 jest.mock('react-native-video', () => 'Video');
 
-jest.mock('react-native-video-cache-control', () => ({
-  __esModule: true,
-  default: () => {},
-}));
+// jest.mock('react-native-video-cache-control', () => ({
+//   __esModule: true,
+//   default: () => {},
+// }));
 
 jest.mock('react-native-reanimated', () => {
   return {
